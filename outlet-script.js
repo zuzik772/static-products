e7004884c4a33374c6680eefb096216220a5f754
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2021-09-05 15:48:03 +0200

    final version

--- outlet-script.js
+++ outlet-script.js
@@ -1,52 +1,59 @@
 // LIST OF PRODUCTS 
-const urlParams = new URLSearchParams(window.location.search);
-const gender = urlParams.get("gender");
-const url = "https://kea-alt-del.dk/t7/api/products/?gender=" + gender;
+// const urlParams = new URLSearchParams(window.location.search);
+const discount = urlParams.get("discount");
+const urlDiscount = "https://kea-alt-del.dk/t7/api/products/?discount";
 
-// const url = "https://kea-alt-del.dk/t7/api/products";
 
-fetch(url)
+
+
+
+
+console.log(urlDiscount);
+fetch(urlDiscount)
     .then(function (response) {
         return response.json()
     })
     .then(function (data) {
-        handleData(data)
+        const productsWithDiscount = data.filter(product => product.discount !== null);
+        handleDiscountData(productsWithDiscount)
     });
 
-function handleData(data) {
-    data.forEach(showProduct)
-    if (gender) {
-        updateHeader();
-    }
+function handleDiscountData(data) {
+    data.forEach(showDiscountedProduct)
+    updateDiscountHeader();
 };
 
-function updateHeader() {
-    document.querySelector("h2.gender").textContent = `${gender}`;
+function updateDiscountHeader() {
+    // console.log(document.querySelector("h2.gender"));
+    document.querySelector("h2.gender").textContent = "Outlet";
+    // console.log(document.querySelector("h2.gender"));
+    // console.log("outlet shows");
+    // document.querySelector("h2.gender").style.display = "none";
 }
 
-function showProduct(product) {
-    console.log(product);
+function showDiscountedProduct(product) {
     // grab the template
     const myTemplate = document.querySelector("template").content;
     // clone it
     const myCLone = myTemplate.cloneNode(true);
     // change content
     myCLone.querySelector("a").setAttribute("href", "product-page.html?id=" + product.id);
-    myCLone.querySelector(".price").textContent = `${product.price} DKK`;
-    // myCLone.querySelector(".discounted").textContent = `${product.discount}`;
+
     myCLone.querySelector(".subtle").textContent = `${product.articletype} | ${product.brandname}`;
     myCLone.querySelector("h3").textContent = `${product.productdisplayname}`;
     myCLone.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
-    if (product.soldout) {
-        myCLone.querySelector("article").classList.add("soldOut");
-    }
-    if (product.discount) {
-        myCLone.querySelector(".price").textContent = `Before: ${product.price} DKK`;
-        myCLone.querySelector("div p").classList.add("onSale");
-        myCLone.querySelector("div p").textContent = `-${product.discount}%`;
-        myCLone.querySelector(".discounted").textContent = `Now: ${Math.ceil(product.price * (1 - product.discount / 100))} DKK`;
 
-    }
+    myCLone.querySelector(".price").textContent = `Before: ${product.price} DKK`;
+    myCLone.querySelector("div p").classList.add("onSale");
+    myCLone.querySelector("div p").textContent = `-${product.discount}%`;
+    myCLone.querySelector(".discounted").textContent = `Now: ${Math.ceil(product.price * (1 - product.discount / 100))} DKK`;
+    // if (product.soldout) {
+    //     myCLone.querySelector("article").classList.add("soldOut");
+    // }
+
+    //  else {
+    //     myCLone.querySelector("article").style.display = "none";
+    // }
     // grab parent
     const parent = document.querySelector("main");
     // append
--- script.js
+++ script.js
@@ -9,15 +9,29 @@ fetch(url)
 // populate the page
 function showProduct(product) {
     console.log(product);
-    document.querySelector(".brand").textContent = product.brandname;
+    document.querySelector(".brand-bio").textContent = product.brandbio;
     document.querySelector(".product-name").textContent = product.productdisplayname;
-    // img
-    //    < img src = "https://kea-alt-del.dk/t7/images/webp/640/1163.webp"
-    //    alt = "Sahara Team India Fanwear Round Neck Jersey" / >
+    document.querySelector(".category").textContent = product.category;
+    document.querySelector(".subcategory").textContent = product.subcategory;
+    document.querySelector(".season").textContent = product.season;
+    document.querySelector(".usage-type").textContent = product.articletype;
+    document.querySelector(".gender").textContent = product.gender;
+    document.querySelector(".type").textContent = product.usagetype;
+    document.querySelector(".brand").textContent = product.brandname;
+
     document.querySelector("img.product-image").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
     document.querySelector("img.product-image").alt = product.productdisplayname;
 
     document.querySelector(".price").textContent = `${product.price}DKK`;
     document.querySelector(".discount").textContent = `-${product.discount}%`;
-    // document.querySelector(".discounted").textContent = ` ${Math.ceil(product.price * (1 - product.discount / 100))} DKK`;
+    document.querySelector(".discounted").textContent = `Now: ${Math.ceil(product.price * (1 - product.discount / 100))} DKK`;
+    if (product.discount) {
+        document.querySelector(".price").textContent = `Before: ${product.price}DKK`;
+        document.querySelector(".price").style.color = "var(--dark-color3)";
+        document.querySelector(".discount").textContent = `-${product.discount}%`;
+    } else {
+        document.querySelector(".discount").textContent = ``;
+        document.querySelector(".discount").remove("onSale");
+        document.querySelector(".discounted").textContent = ` `;
+    }
 }
